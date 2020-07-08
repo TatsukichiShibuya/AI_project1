6428fbc413752c7e50ff5f30597a8009fc7113b3
TatsukichiShibuya <tatsukichi78@gmail.com>
2020-07-09 00:39:08 +0900

    update

--- implemention/Environment.py
+++ implemention/Environment.py
@@ -5,6 +5,7 @@ import pprint
 
 class Environment:
     def __init__(self, setting):
+        # qwerty配列
         self.qwerty = np.array([[16, 22, 4, 17, 19, 24, 20, 8, 14, 15, 28],
                                 [0, 18, 3, 5, 6, 7, 9, 10, 11, 31, 32],
                                 [26, 25, 23, 2, 21, 1, 13, 12, 29, 30, 27]],
@@ -28,7 +29,80 @@ class Environment:
         print("初期配列")
         pprint.pprint(self.board)
 
-    def score(self, board):  # スコア関数(勾配降下用、考え中)
+        # 評価用の文字列
+        self.line = "I've seen horrors horrors that you've seen." +\
+            " But you have no right to call me a murderer." +\
+            " You have a right to kill me." +\
+            " You have a right to do that" +\
+            " but you have no right to judge me." +\
+            " It's impossible for words to describe" +\
+            " what is necessary to those who do not know what horror means." +\
+            " Horror." +\
+            " Horror has a face and you must make a friend of horror." +\
+            " Horror and moral terror are your friends." +\
+            " If they are not then they are enemies to be feared." +\
+            " They are truly enemies." +\
+            " I remember when I was with Special Forces." +\
+            " Seems a thousand centuries ago." +\
+            " We went into a camp to inoculate the children." +\
+            " We left the camp after" +\
+            " we had inoculated the children for Polio" +\
+            " and this old man came running after us and he was crying." +\
+            " He couldn't see." +\
+            " We went back there and they had come and" +\
+            " hacked off every inoculated arm." +\
+            " There they were in a pile." +\
+            " A pile of little arms." +\
+            " And I remember I I I cried." +\
+            " I wept like some grandmother." +\
+            " I wanted to tear my teeth out." +\
+            " I didn't know what I wanted to do." +\
+            " And I want to remember it." +\
+            " I never want to forget it." +\
+            " I never want to forget." +\
+            " And then I realized like I was shot" +\
+            " like I was shot with a diamond" +\
+            " a diamond bullet right through my forehead." +\
+            " And I thought My God the genius of that." +\
+            " The genius." +\
+            " The will to do that." +\
+            " Perfect, genuine, complete, crystalline, pure." +\
+            " And then I realized they were stronger than we." +\
+            " Because they could stand that these were not monsters." +\
+            " These were men trained cadres." +\
+            " These men who fought with their hearts," +\
+            " who had families, who had children," +\
+            " who were filled with love but" +\
+            " they had the strength the strength to do that." +\
+            " If I had ten divisions of those men" +\
+            " our troubles here would be over very quickly." +\
+            " You have to have men who are moral" +\
+            " and at the same time who are able to utilize" +\
+            " their primordial instincts to kill without feeling" +\
+            " without passion without judgment without judgment." +\
+            " Because it's judgment that defeats us."
+        self.line = self.line.lower().split(" ")
+        print("評価文字列")
+        pprint.pprint(self.line)
+
+        # 文字と数字の対応(a:0 z:25 ':26 ,:27 .:28)
+        self.table = np.arange(0, 123)
+        self.table -= ord('a')
+        num = 26
+        for i in ["'", ",", "."]:
+            self.table[ord(i)] = num
+            num += 1
+
+    def score(self, board_map):  # スコア関数(勾配降下用、単語の連続した文字の距離の和)
+        score = 0
+        for word in self.line:
+            for i in range(1, len(word)):
+                a, b = self.table[ord(word[i])], self.table[ord(word[i-1])]
+                score += abs(board_map[a, 0]-board_map[b, 0])**2
+                score += abs(board_map[a, 1]-board_map[b, 1])**2
+        return score
+
+    def heuristic(self, board):  # ヒューリステック関数(A*用、qwertyとの誤差)
         score = 0
         for val in range(33):
             bx = by = qx = qy = 0
@@ -42,6 +116,3 @@ class Environment:
                         qx, qy = i, j
             score -= (abs(qx-bx)**2+abs(qy-by)**2)
         return score
-
-    def heuristic(self, board):  # ヒューリステック関数(A*用、qwertyとの誤差)
-        return len(board[board == self.qwerty])
--- implemention/Model.py
+++ implemention/Model.py
@@ -1,6 +1,27 @@
+import numpy as np
 import time
 import pprint
 import random
+import copy
+
+
+def swap_board(board, board_map,  ij, ij_):
+    val1 = board[ij[0], ij[1]]
+    val2 = board[ij_[0], ij_[1]]
+    board_map[val2] = copy.deepcopy(ij)
+    board_map[val1] = copy.deepcopy(ij_)
+
+    temp = board[ij[0], ij[1]]
+    board[ij[0], ij[1]] = board[ij_[0], ij_[1]]
+    board[ij_[0], ij_[1]] = temp
+
+
+def board2map(board):
+    res = np.zeros((33, 2))
+    for i in range(3):
+        for j in range(11):
+            res[board[i, j]] = [i, j]
+    return res
 
 
 class GradientDescent:
@@ -11,11 +32,69 @@ class GradientDescent:
         etime = executiontime  # 実行時間(この時間回す)
         start = time.time()
         now = start
-        while(now-start < etime):
 
+        # 全ての回中最高のスコア
+        best_board = self.env.board.copy()
+        best_map = board2map(best_board)
+        best_score = self.env.score(best_map)
+
+        # current_score:whileの各回でのはじめのスコア
+        current_board = self.env.board.copy()
+        current_map = board2map(current_board)
+        current_score = self.env.score(current_map)
+
+        count = 0
+
+        while(now-start < etime):
+            count += 1
+
+            swap_list = []
+            # max_score:各スワップで最も良いスコア(はじめはcurrent_score)
+            max_score = current_score
+            for i in range(3):
+                for j in range(10):
+                    # 横のスワップ([i,j]<->[i,j+1])
+                    swap_board(current_board, current_map, [i, j], [i, j+1])
+
+                    score = self.env.score(current_map)
+                    if(score >= max_score):
+                        swap_list.append([[i, j], [i, j+1]])
+                        max_score = score
+
+                    swap_board(current_board, current_map, [i, j], [i, j+1])
+
+            for i in range(2):
+                for j in range(11):
+                    # 縦のスワップ([i,j]<->[i+1,j])
+                    swap_board(current_board, current_map, [i, j], [i+1, j])
+
+                    score = self.env.score(current_map)
+                    if(score >= max_score):
+                        swap_list.append([[i, j], [i+1, j]])
+                        max_score = score
+
+                    swap_board(current_board, current_map, [i, j], [i+1, j])
+
+            if(len(swap_list) != 0):
+                swap = random.choice(swap_list)
+                ij, ij_ = swap[0], swap[1]
+                temp = current_board[ij[0], ij[1]]
+                current_board[ij[0], ij[1]] = current_board[ij_[0], ij_[1]]
+                current_board[ij_[0], ij_[1]] = temp
+                current_score = max_score
+            else:
+                break
+
+            if(count % 10 == 0):
+                print(count, current_score)
             now = time.time()
-        best = self.env.board
-        return best
+
+        print("展開数:", count)
+        if(now - start > etime):
+            print("time over")
+        else:
+            print(now-start)
+        return current_board, current_score
 
 
 class MountainClimbing:
@@ -26,9 +105,6 @@ class MountainClimbing:
         etime = executiontime  # 実行限界時間(この時間で打ち切る)
         start = time.time()
         now = start
-
-        # best_board = self.env.board.copy()
-        # best_score = self.env.score(best_board)
 
         # current_score:whileの各回でのはじめのスコア
         current_board = self.env.board.copy()
@@ -45,36 +121,26 @@ class MountainClimbing:
             for i in range(3):
                 for j in range(10):
                     # 横のスワップ([i,j]<->[i,j+1])
-
-                    temp = current_board[i, j]
-                    current_board[i, j] = current_board[i, j+1]
-                    current_board[i, j+1] = temp
+                    swap_board(current_board, [i, j], [i, j+1])
 
                     score = self.env.score(current_board)
                     if(score >= max_score):
                         swap_list.append([[i, j], [i, j+1]])
                         max_score = score
 
-                    temp = current_board[i, j]
-                    current_board[i, j] = current_board[i, j+1]
-                    current_board[i, j+1] = temp
+                    swap_board(current_board, [i, j], [i, j+1])
 
             for i in range(2):
                 for j in range(11):
                     # 縦のスワップ([i,j]<->[i+1,j])
-
-                    temp = current_board[i, j]
-                    current_board[i, j] = current_board[i+1, j]
-                    current_board[i+1, j] = temp
+                    swap_board(current_board, [i, j], [i+1, j])
 
                     score = self.env.score(current_board)
                     if(score >= max_score):
                         swap_list.append([[i, j], [i+1, j]])
                         max_score = score
 
-                    temp = current_board[i, j]
-                    current_board[i, j] = current_board[i+1, j]
-                    current_board[i+1, j] = temp
+                    swap_board(current_board, [i, j], [i+1, j])
 
             if(len(swap_list) != 0):
                 swap = random.choice(swap_list)
